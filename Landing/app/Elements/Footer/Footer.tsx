b414e1324385a9997aa70ea24802ee0bb4688d2b
DevAffairs <devaffairs.com@gmail.com>
2025-02-28 02:36:51 +0530

    Social Links Added

--- Landing/app/Elements/Footer/Footer.tsx
+++ Landing/app/Elements/Footer/Footer.tsx
@@ -4,6 +4,34 @@ import { FaXTwitter } from 'react-icons/fa6'
 import Image from 'next/image';
 
 function Footer() {
+
+    const socialLinks = [
+        {
+            name: 'Facebook',
+            link: 'https://www.facebook.com/devaffairs.dev',
+            icon: <FaFacebookF />,
+            className: "bg-blue-600 hover:bg-blue-700"
+        },
+        {
+            name: 'Twitter',
+            link: '#',
+            icon: <FaXTwitter />,
+            className: "bg-gray-800 hover:bg-gray-900"
+        },
+        {
+            name: 'Instagram',
+            link: 'https://www.instagram.com/devaffairs.dev',
+            icon: <FaInstagram />,
+            className: "bg-pink-500 hover:bg-pink-600"
+        },
+        {
+            name: 'Linkedin',
+            link: 'https://www.linkedin.com/company/devaffairs',
+            icon: <FaLinkedinIn />,
+            className: "bg-blue-700 hover:bg-blue-800"
+        }
+    ]
+
     return (
         <>
             <footer className="bg-gray-100 p-6 border-t border-gray-200">
@@ -15,18 +43,13 @@ function Footer() {
                             <span className='text-gray-800 text-l font-bold'>DEV AFFAIRS</span>
                         </div>
                         <div className="flex space-x-4">
-                            <a href="#" className="bg-blue-600 p-2 rounded-full text-white hover:bg-blue-700 transition duration-300">
-                                <FaFacebookF />
-                            </a>
-                            <a href="#" className="bg-gray-800 p-2 rounded-full text-white hover:bg-gray-900 transition duration-300">
-                                <FaXTwitter />
-                            </a>
-                            <a href="#" className="bg-pink-500 p-2 rounded-full text-white hover:bg-pink-600 transition duration-300">
-                                <FaInstagram />
-                            </a>
-                            <a href="#" className="bg-blue-700 p-2 rounded-full text-white hover:bg-blue-800 transition duration-300">
-                                <FaLinkedinIn />
-                            </a>
+                            {
+                                socialLinks.map((link, index) => (
+                                    <a href={link.link} className={"p-2 rounded-full text-white transition duration-300 " + link.className} key={index} target="_blank" rel="noopener noreferrer">
+                                        {link.icon}
+                                    </a>
+                                ))
+                            }
                         </div>
                     </div>
                     <hr className="my-4 border-gray-200" />
